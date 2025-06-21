1c32751ea42d8174f3bbbee15f06d6dda31ac846
Rishika-A-S <rishika.as24@gmail.com>
2025-06-21 12:03:16 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wasteDetection',
+    version= '0.0.0',
+    author= 'Rishika  A S',
+    author_email= 'rishika.as24@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
